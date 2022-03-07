fbf37352216675eb67313d68922f96ddabdaaa09
RoderickM1 <100956068+RoderickM1@users.noreply.github.com>
2022-03-07 09:44:13 -0700

    Update pxt.json, main.blocks, main.ts, images.g.jres, images.g.ts

---
+++ images.g.ts
@@ -0,0 +1,37 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "pig":return img`
+. . . . . . . . . . . . . . . . 
+. . 7 7 7 7 . . . 7 7 7 . . . . 
+. 7 7 6 6 7 . . . 7 6 6 7 7 . . 
+. 7 6 7 7 7 7 7 7 7 7 7 6 7 . . 
+. 7 6 7 7 7 7 7 7 7 7 7 7 7 . . 
+. 7 7 7 7 7 7 7 7 7 7 7 7 7 7 . 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 6 6 6 6 6 6 7 7 7 7 7 
+7 7 1 1 f 6 7 7 7 7 6 f 1 1 7 7 
+7 7 1 1 f 6 7 7 7 7 6 f 1 1 7 7 
+7 7 7 7 7 6 6 6 6 6 6 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+. 7 7 7 7 7 7 7 7 7 7 7 7 7 7 . 
+. . 7 7 7 7 7 7 7 7 7 7 7 7 . . 
+. . . . 7 7 7 7 7 7 7 7 7 7 . . 
+. . . . . . . . 7 7 . . . . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
